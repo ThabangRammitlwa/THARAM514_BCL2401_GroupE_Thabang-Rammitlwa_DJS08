a91784329dffa351ea1559f84dbb05e4f3f2a0e5
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-06 15:54:46 +0200

    Create VanDetail.jsx

---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom"
+
+export default function Vans(){
+    const useParams= VanDetail()
+    console.log(params)
+
+    return <h1>Van detail goes here</h1>
+}
