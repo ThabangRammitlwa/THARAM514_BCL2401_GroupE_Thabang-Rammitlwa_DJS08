3fa4fcfcbd007afd7c462bf1c71298d31c78bd90
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-06 13:55:39 +0200

    Route Params - `id`: The ID of the user to retrieve.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import ReactDom from 'react-dom/client';
 import { BrowserRouter,Routes, Route ,Link } from "react-router-dom";
-import Home from "./pages/home"
+import Home from "./pages/Home"
 import About from "./pages/about"
 import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
 
 
 //Import"./server"
@@ -16,12 +17,16 @@ function App() {
       <link classname="site-logo" to="/">#vanLife</link>
       <nav>
         <link to="/about">About</link>
+        <Link to="/vans">Vans</Link>
         </nav>
     </hearder>
     <Routes>
       <Route path="/"elements={<Home/>} />
       <Route path="/"elements={<About/>} />
+      <Route path="/vans" element={<Vans />} />
+      <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
+
       </BrowserRouter>
   )
 }
---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Vans(){
+    return <h1>Van detail goes here</h1>
+}
